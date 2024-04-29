e2420fb02e3868f17b24b541036f8e47fdc99942
Muhammad Bilal Rajput <160275557+starrajput@users.noreply.github.com>
2024-04-29 12:56:11 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+// function add(digit:number , digitTwo:number){
+//     console.log( digit + digitTwo );
+// }
+// add(10 , 56)
+// function Google_Maps(to:string , from:string){
+//     console.log(`${to} ${from}`);
+// }
+// Google_Maps("GH" , "Home")
+// function Google_Map(to:string , from:string){
+//     console.log(`I want to go from ${to} to my ${from}`);
+// }
+// Google_Map("Airport" , "hotel")
+// function Marriage(choice:string = "Ammi ki choice"){
+//     console.log(choice);
+// }
+// Marriage()
+// Marriage("meri choice")
+// function Game(username:string = "guest1234"){
+//     console.log(username);
+// }
+// Game()
+// Game("Ali")
+// function Add(dig:number = 5 , digTwo:number = 2){
+//     console.log(dig + digTwo);
+// }
+// Add(undefined , 21)
+// function Cookmeal(type:string , dishname:string){
+//     return `${type} , ${dishname}`
+// }
+// console.log(Cookmeal("Ckn" , "biryani"));
+// function Country():string {
+//     return "pakistan"
+// }
+// console.log(Country() );
+// function Country(d:number , dTwo: number):number {
+//     return d * dTwo
+// }
+// console.log(Country(10 , 5) );
+// function Name():void {
+//     console.log("Hello");
+// }
+// console.log(Name());
+function Name(name, age) {
+    var Info = "".concat(name, " ").concat(age);
+    return Info;
+}
+console.log(Name("Ali", "22"));
